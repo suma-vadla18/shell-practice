096a416859c83e4d2f2ac82cc8d02bb62d1f5780
suma <suma.vadla18@gmail.com>
2026-01-29 01:10:36 +0530

    actions

---
+++ 02-conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Suresh: Hello Ramesh, How are you?"
+echo "Ramesh: Hello Suresh, I am fine, How are you?"
+echo "Suresh: Doing Fine. What are you learning?"
+echo "Suresh: i am learning shell scripting?"
