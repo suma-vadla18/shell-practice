15256dfb2e573e0aca20420fc3669af7f1850d0f
suma <suma.vadla18@gmail.com>
2026-01-29 01:27:17 +0530

    actions

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1: Hello $PERSON2, How are you?"
+echo "$PERSON2: Hello $PERSON1, I am fine, How are you?"
+echo "$PERSON1: Doing Fine. What are you learning?"
+echo "$PERSON2: i am learning shell scripting?"
