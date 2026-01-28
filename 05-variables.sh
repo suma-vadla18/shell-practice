5360b8f1656eda5bf7ed6b4ba6c83e9e211d5bd5
suma <suma.vadla18@gmail.com>
2026-01-29 01:32:58 +0530

    actions

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-$1
-$2
+echo "$1"
+echo "$2"
 echo "$1: Hello $2, How are you?"
 echo "PERSON2: Hello PERSON1, I am fine, How are you?"
 echo "PERSON1: Doing Fine. What are you learning?"
