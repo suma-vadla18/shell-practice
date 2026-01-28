62585dedaf7891820a981294d70cedc2e9d83639
suma <suma.vadla18@gmail.com>
2026-01-29 01:37:02 +0530

    actions

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo "$1"
-echo "$2"
-echo "$1: Hello $2, How are you?"
-echo "PERSON2: Hello PERSON1, I am fine, How are you?"
-echo "PERSON1: Doing Fine. What are you learning?"
-echo "PERSON2: i am learning shell scripting?"
+echo "Please enter your name:"
+read USER_NAME
+
+echo "username is $USER_NAME"
+echo "please enter your password"
+read -s PASSWD
+echo "entered password is $PASSWD
