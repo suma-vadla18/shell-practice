f5e9d0d132b78166ed58c2145b3ce338c1115c65
suma <suma.vadla18@gmail.com>
2026-01-29 22:22:44 +0530

    message

---
+++ 07.date1.sh
@@ -0,0 +1,14 @@
+#!/bib/bash
+
+START_TIME=$(date +%s)
+
+echo "script executed at: $START_TIME"
+
+sleep 10
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=($($START_TIME-$END_TIME))
+
+echo "script executed for: $TOTAL_TIME seconds"
+
