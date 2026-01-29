8b0e7f72e23959e6caff23d1e79bdcd76a919a80
suma <suma.vadla18@gmail.com>
2026-01-29 23:13:58 +0530

    message

--- 11-Conditions.sh
+++ 11-Conditions.sh
@@ -4,10 +4,10 @@ NUMBER=$1
 if [ $NUMBER -gt 20 ]; then
     echo "Given number:$NUMBER is greater than 20"
 
-elif [ $NUMBER -lt 20]; then 
+elif [ $NUMBER -lt 20 ]; then
     echo "Given number:$NUMBER is less than 20"
 
 else
     echo "Given number:$NUMBER is equal to 20"
-    
+
 fi
