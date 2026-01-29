fefc8650b241954165699bfa11a75c294daac4b8
suma <suma.vadla18@gmail.com>
2026-01-29 23:31:22 +0530

    message

---
+++ 14-installation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+ echo "Please run this script with root user acess"
+ exit 1
+fi
+echo "installing nginx"
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing Nginx...FAILURE"
+else
+    echo "Installing Nginx...SUCCESS"
