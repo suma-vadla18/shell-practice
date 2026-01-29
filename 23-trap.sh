4f1f8a4cad81fc9b8de24fd466dc6bee4b0b8983
suma <suma.vadla18@gmail.com>
2026-01-30 01:13:10 +0530

    message

---
+++ 23-trap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+trap 'echo "there is an error in $LINENO, command:$BASH_COMMAND"' ERR
+
+echo "hello world"
+echo "i am learning shell"
+echoo "printing error here"
+echo "No error in this"
