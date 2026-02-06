149d8901792932353dce4e7e6efc3b0bae45a73e
suma <suma.vadla18@gmail.com>
2026-02-06 23:13:05 +0530

    action

--- 26-delete-old-logs.sh
+++ 26-delete-old-logs.sh
@@ -6,23 +6,15 @@ Y="\e[33m"
 N="\e[0m"
 
 LOGS_DIR=/home/ec2-user/app-logs
-LOGS_FILE="$LOGS_DIR/$(basename "$0").log"
+LOGS_FILE=$LOGS_DIR/$(basename "$0").log"
 
-if [ ! -d $LOGS_DIR ]; then
-    echo -e "$LOGS_DIR does not exitst"
-    exit 1
+echo "logs directory is : $LOGS_DIR"
+echo "logs file is : $LOGS_FILE"
+
+if [ ! -d "$LOGS_DIR"]; then 
+  echo -e "$LOGS_DIR does not exist"
+  exit 1
 fi
 
 FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -mtime +14)
-# echo "$FILES_TO_DELETE"
-
-if [ -z "$FILES_TO_DELETE" ]; then
-    echo "No files older than 14 days to delete."
-  else
-    while IFS= read -r filepath; do
-      # Process each line here
-      echo "Deleting file: $filepath"
-      rm -f $filepath
-      echo "Deleted file: $filepath"
-    done <<< $FILES_TO_DELETE
-fi
+echo "files to delete are: $FILES_TO_DELETE"
