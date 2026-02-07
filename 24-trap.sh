736100ccdb44104179a805fb05a275697a1a449c
suma <suma.vadla18@gmail.com>
2026-02-07 18:27:21 +0530

    action

--- 24-trap.sh
+++ 24-trap.sh
@@ -17,13 +17,14 @@ N="\e[0m"
 if [ $USERID -ne 0 ]; then
  echo -e "$R please run this script root user acess $N" | tee -a $LOG_File
  exit 1
+
 fi
 
 mkdir -p $LOG_FOLDER
 
 for package in $@
 do 
-    dnf list installed $package &>>$LOG_File
+    dnf list installed $package 
     echo "status is: $?"
     if [ $? -ne 0 ]; then
         echo "$package not installed, installing now"
